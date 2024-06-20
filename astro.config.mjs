b0fddf1e428d95b8cca753b59ee89d88b1e03414
mieras <mieras@gmail.com>
2024-06-20 14:07:00 +0200

    mijn dingen

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,23 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
 
-import {loadEnv} from 'vite';
-const {IMAGE_DOMAIN}  = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+import { loadEnv } from 'vite'
+const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
-  image:{
-    domains:[IMAGE_DOMAIN] 
+  image: {
+    domains: [IMAGE_DOMAIN],
   },
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
 })
