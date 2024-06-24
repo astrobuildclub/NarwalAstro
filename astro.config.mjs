cf76141f7e51eb276059a1cd56e9d4195ee40e29
ckdt <ronny@allthis.digital>
2024-06-24 13:11:49 +0200

    First

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+import { loadEnv } from 'vite';
+import react from "@astrojs/react";
+const {
+  IMAGE_DOMAIN
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
-import { loadEnv } from 'vite'
-const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: [IMAGE_DOMAIN],
+    domains: [IMAGE_DOMAIN]
   },
   compressHTML: true,
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
-})
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false
+  }), compress(), react()]
+});
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -19,3 +19,17 @@ export function getCount(parent, getChildrensChildren){
 export function replaceUrls(content, replacementUrl){
   return content.replaceAll(`${import.meta.env.PUBLIC_WP_URL}`, replacementUrl);
 };
+
+export function slugify(input) {
+  if (!input)
+      return '';
+  // make lower case and trim
+  var slug = input.toLowerCase().trim();
+  // remove accents from charaters
+  slug = slug.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  // replace invalid chars with spaces
+  slug = slug.replace(/[^a-z0-9\s-]/g, ' ').trim();
+  // replace multiple spaces or hyphens with a single hyphen
+  slug = slug.replace(/[\s-]+/g, '-');
+  return slug;
+}
