b3859a6b6fefbd9dc1e659ce7ae10dc6031fd160
ckdt <ronny@allthis.digital>
2024-06-26 21:47:29 +0200

    Netlify check readyness deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from 'astro-icon';
 import { loadEnv } from 'vite';
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 const {
   IMAGE_DOMAIN
 } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
@@ -12,6 +14,8 @@ const {
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   image: {
     domains: [IMAGE_DOMAIN]
   },
