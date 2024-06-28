f08b5ba96785ae8c7c9be2978087d4e8631137e3
ckdt <ronny@allthis.digital>
2024-06-28 09:29:30 +0200

    BugFixin

---
+++ .netlify/build/chunks/astro_Dwv35SfV.mjs
@@ -0,0 +1,3 @@
+import 'kleur/colors';
+import './astro/server_N6iVd50k.mjs';
+import 'clsx';
---
+++ .netlify/build/pages/_---uri_.astro.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('../chunks/_...uri__CYLRxHn1.mjs');
+
+export { page };
